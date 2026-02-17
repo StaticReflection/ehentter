e331fb6e436d315858ba7b5c60d49440d9abcd2d
StaticReflection <deng3616888303@gmail.com>
2026-02-18 03:02:18 +0800

    refactor: move MainApp to presentation layer

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
+import 'package:bloc/bloc.dart';
 import 'package:ehentter/core/bloc/app_bloc_observer.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ehentter/core/di/injection_container.dart';
-import 'package:ehentter/l10n/generated/app_localizations.dart';
-import 'package:ehentter/presentation/common/logic/locale/locale_cubit.dart';
-import 'package:ehentter/core/router/app_router.dart';
-import 'package:ehentter/presentation/common/base/app_theme.dart';
-import 'package:ehentter/presentation/common/logic/theme/theme_cubit.dart';
+import 'package:ehentter/presentation/app.dart';
+import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,35 +11,5 @@ void main() async {
 
   Bloc.observer = AppBlocObserver();
 
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<ThemeCubit>(create: (_) => sl()),
-        BlocProvider<LocaleCubit>(create: (_) => sl()),
-      ],
-      child: Builder(
-        builder: (context) {
-          final themeMode = context.watch<ThemeCubit>().state;
-          final locale = context.watch<LocaleCubit>().state;
-
-          return MaterialApp.router(
-            theme: AppTheme.lightTheme,
-            darkTheme: AppTheme.darkTheme,
-            themeMode: themeMode,
-            localizationsDelegates: AppLocalizations.localizationsDelegates,
-            supportedLocales: AppLocalizations.supportedLocales,
-            locale: locale,
-            routerConfig: AppRouter.router,
-          );
-        },
-      ),
-    );
-  }
+  runApp(const App());
 }
---
+++ lib/presentation/app.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ehentter/core/di/injection_container.dart';
+import 'package:ehentter/l10n/generated/app_localizations.dart';
+import 'package:ehentter/presentation/common/logic/locale/locale_cubit.dart';
+import 'package:ehentter/core/router/app_router.dart';
+import 'package:ehentter/presentation/common/base/app_theme.dart';
+import 'package:ehentter/presentation/common/logic/theme/theme_cubit.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ThemeCubit>(create: (_) => sl()),
+        BlocProvider<LocaleCubit>(create: (_) => sl()),
+      ],
+      child: Builder(
+        builder: (context) {
+          final themeMode = context.watch<ThemeCubit>().state;
+          final locale = context.watch<LocaleCubit>().state;
+
+          return MaterialApp.router(
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            themeMode: themeMode,
+            localizationsDelegates: AppLocalizations.localizationsDelegates,
+            supportedLocales: AppLocalizations.supportedLocales,
+            locale: locale,
+            routerConfig: AppRouter.router,
+          );
+        },
+      ),
+    );
+  }
+}
